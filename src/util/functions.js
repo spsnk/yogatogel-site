2f2bf5295b688e681a5465d2d8edec364f27bdc7
Salvador Paz Santos <18468817+spsnk@users.noreply.github.com>
2021-02-05 01:23:38 -0600

    Develop (#3)
    * Better handling of profile read only form fields.\n* Changed primary button color\n*
    
    * Changed account/index to account/profile. Added animated plus sign on menu. Added Play menu
    
    * Added Games structure and xD game
    
    * Added Fee, changed BB for button, changed style for bet inputs
    
    * Changed == for ===
    
    * Fixed bug when adding new bet with auto enabled
    
    * Add BB
    
    * Bet panel and result panel
    
    * Different bet showing
    
    * Changed markets to be received by the component
    
    * Added 3 more games
    
    * Removed extra menu items, added deposit/withdraw placeholder
    
    * Changes
    
    * Removed fee|kei from all games for demo
    
    * Fixed BB with substrings

--- src/util/functions.js
+++ src/util/functions.js
@@ -1,4 +1,4 @@
-export const findPermutations = string => {
+const findPermutations = string => {
   if (!string || typeof string !== "string") {
     return ""
   } else if (string.length < 2) {
@@ -20,3 +20,32 @@ export const findPermutations = string => {
   }
   return permutationsArray
 }
+
+const findSubstrings = (string, size) => {
+  if (!string || typeof string !== "string") {
+    return ""
+  } else if (string.length < size) {
+    return []
+  }
+  let substringsArray = []
+  for (let i = 0; i < string.length - 1; i++) {
+    for (let j = string.length - 1; j - i === size; j--) {
+      substringsArray.push(string.slice(i, j))
+    }
+  }
+
+  return substringsArray
+}
+
+// let array = findPermutations("1234")
+// let combinations = new Set()
+// array.forEach(item => combinations.add(item))
+// for (let number of array) {
+//   //findSubstrings(number, 4).forEach(item => combinations.add(item))
+//   combinations.add(number.slice(0, 2))
+//   combinations.add(number.slice(0, 3))
+// }
+// console.log(Array.from(combinations))
+// console.log(combinations.size)
+
+export { findPermutations, findSubstrings }
